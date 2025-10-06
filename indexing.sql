7cf57babf7f907eb6d45516f97e7b8c52441affc
solresol <gregb@ifost.org.au>
2025-10-06 14:47:49 +1100

    Probably won't make any difference to anything.

---
+++ indexing.sql
@@ -0,0 +1,3 @@
+create index enabled_journals on languageingenetics.journals(enabled);
+create index processed_files on languageingenetics.files(processed);
+create index batches_when_sent on languageingenetics.batches(when_sent);
